45586df4764d8450afa2c952adfb8a62e803045e
Accidit Solutions <acciditsolutions@gmail.com>
2022-08-17 13:47:47 -0300

    Alterando o backgroundo com uma função javasript

--- js/function.js
+++ js/function.js
@@ -1,16 +1,19 @@
 let sinal_vermelho = document.getElementById("vermelho");
 let sinal_amarelo = document.getElementById("amarelo");
 let sinal_verde = document.getElementById("verde");
+let corpo = document.getElementById("body")
 
+let sinal = () => {
     sinal_vermelho.addEventListener("click", () => {
-        alert("Olá sou o sinal vermelho!!");
+        corpo.style.backgroundImage = "url('https://p4.wallpaperbetter.com/wallpaper/1021/284/1018/shanghai-buildings-sky-light-wallpaper-preview.jpg')";
     })
 
     sinal_amarelo.addEventListener("click", () => {
-        alert("Olá sou o sinal amarelo!!");
+        corpo.style.backgroundImage = "url('https://c.pxhere.com/photos/0f/4d/aerial_city_cityscape_cityview_dense_downtown_HD_wallpaper_high_rises-911249.jpg!d')";
     })
 
     sinal_verde.addEventListener("click", () => {
-        alert("Olá sou o sinal verde!!");
+        corpo.style.backgroundImage = "url('https://c0.wallpaperflare.com/preview/276/709/285/brazil-botucatu-clouds-nuvens.jpg')";
     })
-
+}
+sinal();

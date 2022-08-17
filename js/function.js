4e7af2cbd3d0c144de9bebaf9d40f7a20942e4ce
joabegomes <joabegomes605@gmail.com>
2022-08-17 07:30:47 -0300

    Adicionando funções

---
+++ js/function.js
@@ -0,0 +1,16 @@
+let sinal_vermelho = document.getElementById("vermelho");
+let sinal_amarelo = document.getElementById("amarelo");
+let sinal_verde = document.getElementById("verde");
+
+    sinal_vermelho.addEventListener("click", () => {
+        alert("Olá sou o sinal vermelho!!");
+    })
+
+    sinal_amarelo.addEventListener("click", () => {
+        alert("Olá sou o sinal amarelo!!");
+    })
+
+    sinal_verde.addEventListener("click", () => {
+        alert("Olá sou o sinal verde!!");
+    })
+
